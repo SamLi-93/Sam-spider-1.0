03635c99edb8aa918ac1ed9b4428b4cf91bf5e67
SamLi-93 <342447974@qq.com>
2015-06-01 17:42:38 +0800

    wrote a readme file

---
+++ learn.py
@@ -0,0 +1,22 @@
+import urllib
+import re
+
+url = "http://tieba.baidu.com/p/2962968913"
+
+source = urllib.urlopen(url)
+html = source.read()
+
+def getimg(img):
+    reg = r'src="(.+[.jpg])" pic_ext'
+    imgre = re.compile(reg)  
+    
+    l = re.findall(reg, img)
+    x = 0
+    for imgurl in l:  
+        urllib.urlretrieve(imgurl,'D:/%s.jpg' % x)  
+        x = x + 1      
+
+
+
+getimg(html)
+
